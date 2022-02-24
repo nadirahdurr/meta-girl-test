17d4bcc5f790ebff17b0f663c858639e40952320
Nadirah <nadirahdurr@yahoo.com>
2022-02-24 03:48:25 -0500

    updated test contract

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 import abi from "./ContractABI.json";
 
 export const contractABI = abi;
-export const contractAddress = "0x95F6EB6F8aB6a1B870aEa25d84813D8a908127EB";
+export const contractAddress = "0xd218E4bc6372368887Ad10FfB6bF352380804890";
 export const INFURA_ID = "72ed9f10d5c943cca084d16e06879ac0"
