940b919b259f78437ed7ddffadf4bd0ab7b269e9
Nadirah <nadirahdurr@yahoo.com>
2022-03-05 13:41:23 -0500

    updated color of hover button

--- src/sections/Nav.jsx
+++ src/sections/Nav.jsx
@@ -31,7 +31,7 @@ const Nav = () => {
         style={{ fontFamily: "Orbitron" }}
         onClick={connectWallet}
         className="bg-[#231c11] text-[#f2e2d3] md:text-lg text-[12px] font-bold md:w-[160px] md:h-[60px] w-[120px] h-[40px] 
-        rounded-md hover:bg-[#342b17]/90"
+        rounded-md hover:bg-[#342b17]"
       >
         connect wallet
       </button>
@@ -40,7 +40,7 @@ const Nav = () => {
         style={{ fontFamily: "Orbitron" }}
         onClick={disconnectWallet}
         className="bg-[#231c11] md:text-lg text-[12px] text-[#f2e2d3] font-bold md:w-[160px] md:h-[60px] 
-        w-[120px] h-[40px] rounded-md hover:bg-[#342b17]/90"
+        w-[120px] h-[40px] rounded-md hover:bg-[#342b17]"
       >
         {str == undefined
           ? "connect wallet"
